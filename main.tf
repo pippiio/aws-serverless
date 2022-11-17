b6feb545f9f58fa493208d6e398588d0ba5d6f30
Sofus Hilfling <sofus@efio.dk>
2022-11-17 13:52:29 +0100

    removed use of module_variable_optional_attrs

--- main.tf
+++ main.tf
@@ -1,8 +1,5 @@
 locals {
-  config = defaults(var.config, {
-
-  })
-
+  config = var.config
   create_kms_key = local.config.kms_key_id == null ? 1 : 0
   kms_key_id     = try(one(aws_kms_key.this).id, local.config.kms_key_id)
 }
