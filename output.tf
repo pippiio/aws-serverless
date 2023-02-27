8ea2b48bd49df11070510a539c4a84fe7ebb262a
Andreas Vikke <andreasvikke@gmail.com>
2023-02-27 14:12:32 +0100

    Addes secret output

--- output.tf
+++ output.tf
@@ -9,3 +9,11 @@ output "kms_arn" {
 output "api_endpoint" {
   value = local.enable_api_gateway == 1 ? one(aws_apigatewayv2_api.this).api_endpoint : null
 }
+
+ output "secret" {
+  value = {
+    key   = "x-${local.name_prefix}secret"
+    value = sensitive(random_password.this.result)
+  }
+  sensitive = true
+}
