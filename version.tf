c70c401a9d5d72cbaee1b7569e3c7dcba2455e2e
Sofus Hilfling <sofus@techchapter.com>
2024-01-16 11:33:38 +0100

    update versions

--- version.tf
+++ version.tf
@@ -1,10 +1,10 @@
 terraform {
-  required_version = ">= 1.2.0"
+  required_version = ">= 1.5.0"
 
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 4.0"
+      version = "~> 5.32"
     }
     random = {
       source  = "hashicorp/random"
