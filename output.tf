9ead47dcd8c218aa97652c5187aebbd72702d5e0
Jack Hansen <jackvwh@hotmail.com>
2025-06-10 13:57:58 +0200

    feat: add custom_authorizer output and variable

--- output.tf
+++ output.tf
@@ -16,6 +16,13 @@ output "rest_api" {
   }
 }
 
+output "lambda_functions" {
+  value = {
+    for name, func in aws_lambda_function.function :
+    name => { function_name = name, arn = func.arn }
+  }
+}
+
 # output "https_api_endpoint" {
 #   value = local.enable_https_api_gateway == 1 ? one(aws_apigatewayv2_api.this).api_endpoint : null
 # }
